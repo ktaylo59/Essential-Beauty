f1970a2f0565c12bdc42e019b24e9fdabd6195c6
ktaylo59 <ktaylo59@gmail.com>
2020-09-12 13:10:19 -0500

    add cart icon

--- essentialbeauty/src/Components/Navbar.js
+++ essentialbeauty/src/Components/Navbar.js
@@ -2,12 +2,15 @@ import React from "react";
 
 function Navbar() {
   return (
+  
     <div>
+        <h1>ESSENTIAL BEAUTY</h1>
       <nav>
         <ul>
           <li>Lips</li>
           <li>Eyes</li>
           <li>Face</li>
+          <li><i class ="fa fa-shopping-cart">Cart</i></li>
         </ul>
       </nav>
     </div>
