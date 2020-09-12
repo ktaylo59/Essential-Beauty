5c4992594f2462806ca50acf31ca2d96e589d65c
ktaylo59 <ktaylo59@gmail.com>
2020-09-12 12:03:57 -0500

    update Navbar

--- essentialbeauty/src/Components/Navbar.js
+++ essentialbeauty/src/Components/Navbar.js
@@ -1,2 +1,17 @@
-import React from 'react';
+import React from "react";
 
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Lips</li>
+          <li>Eyes</li>
+          <li>Face</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
