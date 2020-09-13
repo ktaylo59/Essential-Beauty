3c19bc844e3d523283385edea308ccdfb905ca8f
ktaylo59 <ktaylo59@gmail.com>
2020-09-13 12:11:37 -0500

    add bootstrap

--- essentialbeauty/src/App.js
+++ essentialbeauty/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -17,17 +18,23 @@ class App extends React.Component {
 //api request
   componentDidMount() {
     fetch('http://makeup-api.herokuapp.com/api/v1/products.json')
-    .then(response => response.json()) 
+    .then(res => res.json()) 
     .then((data) => {
-      this.setState({data})
+      this.setState({data}) 
+      //sets the value of state to the output from the API call
     })
+    .catch(console.log)
   }
    
   render() {
+    const imagesList = this.state.data.map((url, i) => {
+      return <ProductImages key={i} url={url} />
+    })
      return (
     <>
     <Navbar />
     <ProductImages/>
+    {imagesList}
     <Footer />
     </>
       );
--- essentialbeauty/src/Components/ProductImages.js
+++ essentialbeauty/src/Components/ProductImages.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function ProductImages(props) {
     return(
+        <div>
         <img src={props.url} alt="pics"/>
+        </div>
     )
 }
 export default ProductImages;
