9bb4675032f395008b0d05911705ee9a24a6bc18
ktaylo59 <ktaylo59@gmail.com>
2020-09-15 21:31:42 -0500

    add style to wrapper

--- essentialbeauty/src/Components/Lipstick.js
+++ essentialbeauty/src/Components/Lipstick.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Lipstick = (props) => {
         return(
-        <div className="lippie">
-           <img src={props.lipstick.api_featured_image} alt="images"/>
-              <>
+        <div className="product-wrapper">
+                <div className="lippie-wrapper">
+                  <div>
+           <img src={props.lipstick.api_featured_image} alt="images"/></div>
+              <div>
               <h6>{props.lipstick.name}</h6> 
                <p> {props.lipstick.product_type}</p>
-              <p>  {props.lipstick.price} </p>
-                </>
+               <p> {props.lipstick.price}</p>
+                </div>
+                <button>Add to Cart</button>
+                </div>
         </div>
         
         );
