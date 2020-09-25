12d06f82466f1844c5989e8a28287ae65da89102
ktaylo59 <ktaylo59@gmail.com>
2020-09-24 19:41:25 -0500

    Updates

---
+++ precache-manifest.a449c1881b6d52530470817dcc751759.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3936bbbfe53fe42bdfa135b6af57bed5",
+    "url": "/Essential-Beauty/index.html"
+  },
+  {
+    "revision": "cea92c96d52a965f0b47",
+    "url": "/Essential-Beauty/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "11741b15df1d158a9ab1",
+    "url": "/Essential-Beauty/static/css/main.44f7c1d9.chunk.css"
+  },
+  {
+    "revision": "cea92c96d52a965f0b47",
+    "url": "/Essential-Beauty/static/js/2.8b7b3394.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Essential-Beauty/static/js/2.8b7b3394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11741b15df1d158a9ab1",
+    "url": "/Essential-Beauty/static/js/main.c9182c2e.chunk.js"
+  },
+  {
+    "revision": "67e906be0e6e06ef04db",
+    "url": "/Essential-Beauty/static/js/runtime-main.164adfd9.js"
+  },
+  {
+    "revision": "29bf54561ee35c6d0d46c93decc477bb",
+    "url": "/Essential-Beauty/static/media/Eyes.29bf5456.jpg"
+  },
+  {
+    "revision": "9081c638ad8a4cc09b458689604a4c9c",
+    "url": "/Essential-Beauty/static/media/Face.9081c638.jpg"
+  },
+  {
+    "revision": "febf51ee2a4ed148e8287c030e187cfc",
+    "url": "/Essential-Beauty/static/media/Lips.febf51ee.jpg"
+  }
+]);
